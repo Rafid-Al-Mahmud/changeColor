9d2c8d5eb22a894c2cd91839fd4b22e438180881
Rafid-Al-Mahmud <76583107+Rafid-Al-Mahmud@users.noreply.github.com>
2022-11-22 23:11:38 +0600

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,99 @@ app.get("/data", (req, res) => {
     res.sendFile(__dirname + "/example.json");
 });
 
-app.get("/register", (req, res) => {
+app.get("/load/register", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/register", (req, res) => {
+  res.send(`
+  
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        *{
+            transition: all 1s;
+        }
+        .loader-container {
+            background-color: #000000;
+            color: gold;
+            /*display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            align-items: center;*/
+            font-size: 20px;
+            height: 100vh;
+            overflow: hidden;
+            font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            animation: font-opacity-down-up 1s;
+            animation-delay: 1s;
+            animation-iteration-count: initial;
+        }
+        .loader {
+            animation: a1 1s;
+            animation-delay: 3s;
+            animation-iteration-count: infinite;
+            padding: 5px;
+            border-radius: 10px;
+            box-shadow: 0 0 5px 0 gold;
+            
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+        }
+
+        @keyframes a1 {
+            0% {
+                opacity: 1;
+            }
+            50% {
+                opacity: 1;
+            }
+            100% {
+                opacity: 0.1;
+            }
+        }
+
+        @keyframes font-opacity-down-up {
+            0% {
+                font-size: 20px;
+            }
+            50% {
+                font-size: 30px;
+            }
+            100% {
+                font-size: 20px;
+            }
+        }
+    </style>
+</head>
+
+<body class="loader-container">
+    <h1 class="loader">MINISOTE</h1>
+    <script>
+            function loadDoc(x) {
+                const xhttp = new XMLHttpRequest();
+                xhttp.onload = function() {
+                    var data = this.responseText;
+                     document.querySelector("*").innerHTML = data;
+                }
+                xhttp.open("GET", x);
+                xhttp.send();
+            }
+            setTimeout(() => {loadDoc("https://www.w3schools.com/js/ajax_info.txt")}, 5000);
+    </script>
+</body>
+
+</html>
+  `);
 });
 
 app.post("/register", (req, res) => {
